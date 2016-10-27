48c4485b9f220974f218541ec5e24887af9ad346
Wicky <codeWicky@163.com>
2016-10-27 15:41:22 +0800

    补充三角函数相关

--- DecimalExtension/DecimalExtension.h
+++ DecimalExtension/DecimalExtension.h
@@ -28,5 +28,45 @@ CGFloat floorX(CGFloat x,int y);
 /*
  返回10的x次方
  */
-int pow10(int x);
+int pow10x(int x);
+/**
+ 返回x的平方
+ */
+CGFloat powX2(CGFloat x);
+/**
+ 返回x的立方
+ */
+CGFloat powX3(CGFloat x);
+/**
+ 返回平均数
+ */
+CGFloat avr(CGFloat x,CGFloat y);
+/**
+ 角度转弧度
+ */
+CGFloat radianFromDegree(CGFloat degree);
+/**
+ 弧度转角度
+ */
+CGFloat degreeFromRadian(CGFloat radian);
+/**
+ 余弦定理计算角度
+ a、b:夹边
+ c:对边
+ 
+ 若返回-1则说明当前参数无法构成三角形
+ */
+CGFloat angleFromCosinesLaw(CGFloat a,CGFloat b,CGFloat c);
+/**
+ 余弦定理计算对边
+ a、b:夹边
+ alpha:夹角
+ 
+ 若返回-1则说明夹角数值不合法
+ */
+CGFloat lengthFromCosinesLaw(CGFloat a,CGFloat b,CGFloat alpha);
+/**
+ 返回点2到点1连线与x轴正方向夹角
+ */
+CGFloat angleFromTwoPoint(CGFloat x1,CGFloat y1,CGFloat x2,CGFloat y2);
 @end
--- DecimalExtension/DecimalExtension.m
+++ DecimalExtension/DecimalExtension.m
@@ -10,15 +10,67 @@
 
 @implementation DecimalExtension
 CGFloat roundX(CGFloat x,int y){
-    return roundf(x * pow10(y)) * 1.0 / pow10(y);
+    return roundf(x * pow10x(y)) * 1.0 / pow10x(y);
 }
 CGFloat ceilX(CGFloat x,int y){
-    return ceilf(x * pow10(y)) * 1.0 / pow10(y);
+    return ceilf(x * pow10x(y)) * 1.0 / pow10x(y);
 }
 CGFloat floorX(CGFloat x,int y){
-    return floorf(x * pow10(y)) * 1.0 / pow10(y);
+    return floorf(x * pow10x(y)) * 1.0 / pow10x(y);
 }
-int pow10(int x){
+int pow10x(int x){
     return pow(10, x);
+}
+CGFloat powX2(CGFloat x){
+    return pow(x, 2);
+}
+CGFloat powX3(CGFloat x){
+    return pow(x, 3);
+}
+CGFloat avr(CGFloat x,CGFloat y){
+    return (x + y) / 2.0;
+}
+CGFloat radianFromDegree(CGFloat degree){
+    return degree / 180.0 * M_PI;
+}
+CGFloat degreeFromRadian(CGFloat radian){
+    return radian / M_PI * 180.0;
+}
+CGFloat angleFromCosinesLaw(CGFloat a,CGFloat b,CGFloat c){
+    CGFloat min1 = MIN(a, b);
+    CGFloat max1 = MAX(a, b);
+    CGFloat min2 = MIN(max1, c);
+    CGFloat max = MAX(max1, c);
+    if (min1 + min2 <= max) {
+        return -1;
+    }
+    return acosf((powX2(a) + powX2(b) - powX2(c)) / (2 * a * b));
+}
+CGFloat lengthFromCosinesLaw(CGFloat a,CGFloat b,CGFloat alpha){
+    if (alpha < 0 || alpha > M_PI_2) {
+        return -1;
+    }
+    return sqrtf(powX2(a) + powX2(b) - 2 * a * b * cosf(alpha));
+}
+CGFloat angleFromTwoPoint(CGFloat x1,CGFloat y1,CGFloat x2,CGFloat y2){
+    CGFloat deltaX = x2 - x1;
+    CGFloat deltaY = y2 - y1;
+    if (deltaY > 0) {
+        return acosf(deltaX / deltaY);
+    }
+    else if (deltaY == 0)
+    {
+        if (deltaX >= 0) {
+            return 0;
+        }
+        else
+        {
+            return M_PI;
+        }
+    }
+    else
+    {
+        return M_PI + acosf(deltaX / deltaY);
+    }
 }
 @end
