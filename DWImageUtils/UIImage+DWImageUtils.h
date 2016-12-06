b58417d208e05d3e5ea4b4913625c31df693312d
Wicky <codeWicky@163.com>
2016-12-06 18:44:55 +0800

    添加DWImageUtils

---
+++ DWImageUtils/UIImage+DWImageUtils.h
@@ -0,0 +1,81 @@
+//
+//  UIImage+DWImageUtils.h
+//  Image
+//
+//  Created by Wicky on 2016/12/6.
+//  Copyright © 2016年 Wicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger,DWContentMode)//图片填充模式
+{
+    DWContentModeScaleAspectFit,//适应模式
+    DWContentModeScaleAspectFill,//填充模式
+    DWContentModeScaleToFill//拉伸模式
+};
+@interface UIImage (DWImageUtils)
+///获取带圆角的图片
+/*
+ radius:返回图片的圆角半径
+ 圆角半径不可超过图片尺寸的1/2,否则按1/2处理
+ 
+ width:返回图片的宽度
+ 返回的图片为一个宽高相等的矩形区域，但图片且居中显示
+ 
+ mode:返回图片的填充模式
+ 适应模式:以原图片比例，能显示全部图片的最大尺寸进行填充
+ 填充模式:以原图片比例，图片能充满容器的最小尺寸进行填充
+ 拉伸模式:以拉伸图片能够使图片充满容器的尺寸进行填充
+ */
+-(UIImage *)dw_CornerRadius:(CGFloat)radius withWidth:(CGFloat)width contentMode:(DWContentMode)mode;
+
+///获取旋转角度的图片
+/**
+ 注:角度计数单位为弧度制
+ */
+-(UIImage *)dw_RotateImageWithAngle:(CGFloat)angle;
+
+
+///以灰色空间生成图片
+-(UIImage *)dw_ConvertToGrayImage;
+
+///取图片某点颜色
+/**
+ point:取色点
+ 
+ 注:以图片自身宽高作为坐标系
+ */
+-(UIColor *)dw_ColorAtPoint:(CGPoint)point;
+
+#pragma mark ---以下代码来自网络---
+///纠正图片方向
+-(UIImage *)dw_FixOrientation;
+
+///按给定的方向旋转图片
+-(UIImage*)dw_RotateWithOrient:(UIImageOrientation)orient;
+
+///垂直翻转
+-(UIImage *)dw_FlipVertical;
+
+///水平翻转
+-(UIImage *)dw_FlipHorizontal;
+
+///截取当前image对象rect区域内的图像
+-(UIImage *)dw_SubImageWithRect:(CGRect)rect;
+
+///压缩图片至指定尺寸
+-(UIImage *)dw_RescaleImageToSize:(CGSize)size;
+
+///压缩图片至指定像素
+-(UIImage *)dw_RescaleImageToPX:(CGFloat )toPX;
+
+///在指定的size里面生成一个平铺的图片
+-(UIImage *)dw_GetTiledImageWithSize:(CGSize)size;
+
+///UIView转化为UIImage
++(UIImage *)imageFromView:(UIView *)view;
+
+///将两个图片生成一张图片
++(UIImage*)mergeImage:(UIImage*)firstImage withImage:(UIImage*)secondImage;
+@end
---
+++ DWImageUtils/UIImage+DWImageUtils.m
@@ -0,0 +1,440 @@
+//
+//  UIImage+DWImageUtils.m
+//  Image
+//
+//  Created by Wicky on 2016/12/6.
+//  Copyright © 2016年 Wicky. All rights reserved.
+//
+
+#import "UIImage+DWImageUtils.h"
+
+@implementation UIImage (DWImageUtils)
+-(UIImage *)dw_CornerRadius:(CGFloat)radius withWidth:(CGFloat)width contentMode:(DWContentMode)mode
+{
+    CGFloat originScale = self.size.width / self.size.height;
+    CGFloat height = width / originScale;
+    CGFloat scale = [UIScreen mainScreen].scale;
+    CGFloat maxV = MAX(width, height);
+    if (radius < 0) {
+        radius = 0;
+    }
+    UIImage * image = nil;
+    CGRect imageFrame;
+    if (mode == DWContentModeScaleAspectFit) {//根据图片填充模式制定绘制frame
+        if (originScale > 1) {//适应模式
+            imageFrame = CGRectMake(0, (width - height) / 2, width,height);
+        }
+        else
+        {
+            imageFrame = CGRectMake((height - width) / 2, 0, width, height);
+        }
+    }
+    else if (mode == DWContentModeScaleAspectFill)//填充模式
+    {
+        CGFloat newHeight;
+        CGFloat newWidth;
+        if (originScale > 1) {
+            newHeight = width;
+            newWidth = newHeight * originScale;
+            imageFrame = CGRectMake( -(newWidth - newHeight) / 2, 0, newWidth, newHeight);
+        }
+        else
+        {
+            newWidth = height;
+            newHeight = newWidth / originScale;
+            imageFrame = CGRectMake(0, - (newHeight - newWidth) / 2, newWidth, newHeight);
+        }
+    }
+    else//拉伸模式
+    {
+        imageFrame = CGRectMake(0, 0, maxV, maxV);
+    }
+    UIGraphicsBeginImageContextWithOptions(CGSizeMake(maxV, maxV), NO, scale);//以最大长度开启图片上下文
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [[UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, maxV, maxV) cornerRadius:radius] addClip];//绘制一个圆形的贝塞尔曲线，并做遮罩
+    [self drawInRect:imageFrame];//在指定的frame中绘制图片
+    CGContextRotateCTM(context, M_PI_2);
+    image = UIGraphicsGetImageFromCurrentImageContext();//从当前上下文中获取图片
+    UIGraphicsEndImageContext();//关闭上下文
+    return image;
+}
+
+-(UIImage *)dw_RotateImageWithAngle:(CGFloat)angle
+{
+    UIView *rotatedViewBox = [[UIView alloc] initWithFrame:CGRectMake(0,0,self.size.width, self.size.height)];
+    CGAffineTransform t = CGAffineTransformMakeRotation(angle);
+    rotatedViewBox.transform = t;
+    CGSize rotatedSize = rotatedViewBox.frame.size;
+    
+    ///保证开启最大正方形，防止变形
+    CGFloat length = MAX(rotatedSize.width, rotatedSize.height);
+    rotatedSize = CGSizeMake(length, length);
+    
+    ///开启上下文
+    UIGraphicsBeginImageContextWithOptions(rotatedSize, NO, [UIScreen mainScreen].scale);
+    CGContextRef bitmap = UIGraphicsGetCurrentContext();
+    
+    ///移动原点至图片中心
+    CGContextTranslateCTM(bitmap, rotatedSize.width/2.0, rotatedSize.height/2.0);
+    CGContextScaleCTM(bitmap, 1.0, -1.0);
+    CGContextRotateCTM(bitmap, angle);
+    CGContextDrawImage(bitmap, CGRectMake(-self.size.width / 2, -self.size.height / 2, self.size.width, self.size.height), [self CGImage]);
+    
+    ///旋转
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+-(UIImage *)dw_ConvertToGrayImage
+{
+    int width = self.size.width;
+    int height = self.size.height;
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    CGContextRef context = CGBitmapContextCreate(nil,width,height,8,0,colorSpace,kCGImageAlphaNone);
+    CGColorSpaceRelease(colorSpace);
+    
+    if (context == NULL)
+    {
+        return nil;
+    }
+    
+    CGContextDrawImage(context,CGRectMake(0, 0, width, height), self.CGImage);
+    CGImageRef contextRef = CGBitmapContextCreateImage(context);
+    UIImage *grayImage = [UIImage imageWithCGImage:contextRef];
+    CGContextRelease(context);
+    CGImageRelease(contextRef);
+    
+    return grayImage;
+}
+
+-(UIColor *)dw_ColorAtPoint:(CGPoint)point;
+{
+    if (!CGRectContainsPoint(CGRectMake(0.0f, 0.0f, self.size.width, self.size.height), point))
+    {
+        return nil;
+    }
+    
+    NSInteger pointX = trunc(point.x);
+    NSInteger pointY = trunc(point.y);
+    CGImageRef cgImage = self.CGImage;
+    NSUInteger width = self.size.width;
+    NSUInteger height = self.size.height;
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    int bytesPerPixel = 4;
+    int bytesPerRow = bytesPerPixel * 1;
+    NSUInteger bitsPerComponent = 8;
+    unsigned char pixelData[4] = { 0, 0, 0, 0 };
+    
+    ///创建1*1画布
+    CGContextRef context = CGBitmapContextCreate(pixelData,1,1,bitsPerComponent,bytesPerRow,colorSpace,kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    CGContextSetBlendMode(context, kCGBlendModeCopy);
+    
+    CGContextTranslateCTM(context, -pointX, pointY-(CGFloat)height);
+    ///绘图
+    CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
+    CGContextRelease(context);
+    
+    ///取色
+    CGFloat red   = (CGFloat)pixelData[0] / 255.0f;
+    CGFloat green = (CGFloat)pixelData[1] / 255.0f;
+    CGFloat blue  = (CGFloat)pixelData[2] / 255.0f;
+    CGFloat alpha = (CGFloat)pixelData[3] / 255.0f;
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
+-(UIImage *)dw_FixOrientation
+{
+    if (self.imageOrientation == UIImageOrientationUp) return self;
+    
+    // We need to calculate the proper transformation to make the image upright.
+    // We do it in 2 steps: Rotate if Left/Right/Down, and then flip if Mirrored.
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    
+    switch (self.imageOrientation)
+    {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, self.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+            
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, self.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+    
+    switch (self.imageOrientation)
+    {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, self.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+    
+    // Now we draw the underlying CGImage into a new context, applying the transform
+    // calculated above.
+    CGContextRef ctx = CGBitmapContextCreate(NULL, self.size.width, self.size.height,
+                                             CGImageGetBitsPerComponent(self.CGImage), 0,
+                                             CGImageGetColorSpace(self.CGImage),
+                                             CGImageGetBitmapInfo(self.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    
+    switch (self.imageOrientation)
+    {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.height,self.size.width), self.CGImage);
+            break;
+            
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,self.size.width,self.size.height), self.CGImage);
+            break;
+    }
+    
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    
+    return img;
+}
+
+///按给定的方向旋转图片
+-(UIImage*)dw_RotateWithOrient:(UIImageOrientation)orient
+{
+    CGRect bnds = CGRectZero;
+    CGRect rect = CGRectZero;
+    CGAffineTransform tran = CGAffineTransformIdentity;
+    
+    rect.size = self.size;
+    
+    bnds = rect;
+    
+    switch (orient)
+    {
+        case UIImageOrientationUp:
+            return self;
+            
+        case UIImageOrientationUpMirrored:
+            tran = CGAffineTransformMakeTranslation(rect.size.width, 0.0);
+            tran = CGAffineTransformScale(tran, -1.0, 1.0);
+            break;
+            
+        case UIImageOrientationDown:
+            tran = CGAffineTransformMakeTranslation(rect.size.width,
+                                                    rect.size.height);
+            tran = CGAffineTransformRotate(tran, M_PI);
+            break;
+            
+        case UIImageOrientationDownMirrored:
+            tran = CGAffineTransformMakeTranslation(0.0, rect.size.height);
+            tran = CGAffineTransformScale(tran, 1.0, -1.0);
+            break;
+            
+        case UIImageOrientationLeft:
+            bnds = swapWidthAndHeight(bnds);
+            tran = CGAffineTransformMakeTranslation(0.0, rect.size.width);
+            tran = CGAffineTransformRotate(tran, 3.0 * M_PI / 2.0);
+            break;
+            
+        case UIImageOrientationLeftMirrored:
+            bnds = swapWidthAndHeight(bnds);
+            tran = CGAffineTransformMakeTranslation(rect.size.height,
+                                                    rect.size.width);
+            tran = CGAffineTransformScale(tran, -1.0, 1.0);
+            tran = CGAffineTransformRotate(tran, 3.0 * M_PI / 2.0);
+            break;
+            
+        case UIImageOrientationRight:
+            bnds = swapWidthAndHeight(bnds);
+            tran = CGAffineTransformMakeTranslation(rect.size.height, 0.0);
+            tran = CGAffineTransformRotate(tran, M_PI / 2.0);
+            break;
+            
+        case UIImageOrientationRightMirrored:
+            bnds = swapWidthAndHeight(bnds);
+            tran = CGAffineTransformMakeScale(-1.0, 1.0);
+            tran = CGAffineTransformRotate(tran, M_PI / 2.0);
+            break;
+            
+        default:
+            return self;
+    }
+    
+    UIGraphicsBeginImageContext(bnds.size);
+    CGContextRef ctxt = UIGraphicsGetCurrentContext();
+    
+    switch (orient)
+    {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            CGContextScaleCTM(ctxt, -1.0, 1.0);
+            CGContextTranslateCTM(ctxt, -rect.size.height, 0.0);
+            break;
+            
+        default:
+            CGContextScaleCTM(ctxt, 1.0, -1.0);
+            CGContextTranslateCTM(ctxt, 0.0, -rect.size.height);
+            break;
+    }
+    
+    CGContextConcatCTM(ctxt, tran);
+    CGContextDrawImage(UIGraphicsGetCurrentContext(), rect, self.CGImage);
+    
+    UIImage* copy  = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return copy;
+}
+
+/** 垂直翻转 */
+- (UIImage *)dw_FlipVertical
+{
+    return [self dw_RotateWithOrient:UIImageOrientationDownMirrored];
+}
+
+/** 水平翻转 */
+- (UIImage *)dw_FlipHorizontal
+{
+    return [self dw_RotateWithOrient:UIImageOrientationUpMirrored];
+}
+
+#pragma mark - 截取当前image对象rect区域内的图像
+- (UIImage *)dw_SubImageWithRect:(CGRect)rect
+{
+    CGImageRef newImageRef = CGImageCreateWithImageInRect(self.CGImage, rect);
+    
+    UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
+    
+    CGImageRelease(newImageRef);
+    
+    return newImage;
+}
+
+#pragma mark - 压缩图片至指定尺寸
+- (UIImage *)dw_RescaleImageToSize:(CGSize)size
+{
+    CGRect rect = (CGRect){CGPointZero, size};
+    
+    UIGraphicsBeginImageContext(rect.size);
+    
+    [self drawInRect:rect];
+    
+    UIImage *resImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    return resImage;
+}
+
+#pragma mark - 压缩图片至指定像素
+- (UIImage *)dw_RescaleImageToPX:(CGFloat )toPX
+{
+    CGSize size = self.size;
+    
+    if(size.width <= toPX && size.height <= toPX)
+    {
+        return self;
+    }
+    
+    CGFloat scale = size.width / size.height;
+    
+    if(size.width > size.height)
+    {
+        size.width = toPX;
+        size.height = size.width / scale;
+    }
+    else
+    {
+        size.height = toPX;
+        size.width = size.height * scale;
+    }
+    
+    return [self dw_RescaleImageToSize:size];
+}
+
+#pragma mark - 指定大小生成一个平铺的图片
+- (UIImage *)dw_GetTiledImageWithSize:(CGSize)size
+{
+    UIView *tempView = [[UIView alloc] init];
+    tempView.bounds = (CGRect){CGPointZero, size};
+    tempView.backgroundColor = [UIColor colorWithPatternImage:self];
+    
+    UIGraphicsBeginImageContext(size);
+    [tempView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *bgImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return bgImage;
+}
+
+#pragma mark - UIView转化为UIImage
++(UIImage *)dw_ImageFromView:(UIView *)view
+{
+    CGFloat scale = [UIScreen mainScreen].scale;
+    UIGraphicsBeginImageContextWithOptions(view.frame.size, NO, scale);
+    [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+#pragma mark - 将两个图片生成一张图片
++(UIImage*)dw_MergeImage:(UIImage*)firstImage withImage:(UIImage*)secondImage
+{
+    CGImageRef firstImageRef = firstImage.CGImage;
+    CGFloat firstWidth = CGImageGetWidth(firstImageRef);
+    CGFloat firstHeight = CGImageGetHeight(firstImageRef);
+    CGImageRef secondImageRef = secondImage.CGImage;
+    CGFloat secondWidth = CGImageGetWidth(secondImageRef);
+    CGFloat secondHeight = CGImageGetHeight(secondImageRef);
+    CGSize mergedSize = CGSizeMake(MAX(firstWidth, secondWidth), MAX(firstHeight, secondHeight));
+    UIGraphicsBeginImageContext(mergedSize);
+    [firstImage drawInRect:CGRectMake(0, 0, firstWidth, firstHeight)];
+    [secondImage drawInRect:CGRectMake(0, 0, secondWidth, secondHeight)];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+///交换宽和高
+static CGRect swapWidthAndHeight(CGRect rect)
+{
+    CGFloat swap = rect.size.width;
+    rect.size.width = rect.size.height;
+    rect.size.height = swap;
+    return rect;
+}
+@end
