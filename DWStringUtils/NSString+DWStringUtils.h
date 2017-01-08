60951bedeac466eda6407a49ad418f9db2c80c1f
Wicky <codewicky@163.com>
2017-01-08 21:24:49 +0800

    添加DWStringUtils

---
+++ DWStringUtils/NSString+DWStringUtils.h
@@ -0,0 +1,13 @@
+//
+//  NSString+DWStringUtils.h
+//  RegExp
+//
+//  Created by Wicky on 17/1/8.
+//  Copyright © 2017年 Wicky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (DWStringUtils)
++(NSString *)stringWithMetaString:(NSString *)meta count:(NSUInteger)count;
+@end
