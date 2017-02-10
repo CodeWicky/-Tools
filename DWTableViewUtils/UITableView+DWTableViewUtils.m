e296dd108dadc09d46f51d2ff529bd4ffccfc4df
Wicky <codeWicky@163.com>
2017-02-10 23:41:57 +0800

    DWTableViewUtils相关

--- DWTableViewUtils/UITableView+DWTableViewUtils.m
+++ DWTableViewUtils/UITableView+DWTableViewUtils.m
@@ -7,6 +7,7 @@
 //
 
 #import "UITableView+DWTableViewUtils.h"
+#import <objc/runtime.h>
 
 @implementation UITableView (DWTableViewUtils)
 -(void)reloadDataWithCompletion:(void(^)())completion
@@ -22,4 +23,51 @@
         });
     });
 }
+
+-(void)showPlaceHolderView
+{
+    if (self.placeHolderView && !self.placeHolderView.superview) {
+        [self addSubview:self.placeHolderView];
+    }
+}
+
+-(void)hidePlaceHolderView
+{
+    if (self.placeHolderView && self.placeHolderView.superview) {
+        [self.placeHolderView removeFromSuperview];
+    }
+}
+
+-(void)reloadDataAndHandlePlaceHolderView
+{
+    __weak typeof(self)weakSelf = self;
+    [self reloadDataWithCompletion:^{
+        if ([weakSelf calculateHasData]) {
+            [weakSelf hidePlaceHolderView];
+        } else {
+            [weakSelf showPlaceHolderView];
+        }
+    }];
+}
+
+-(BOOL)calculateHasData
+{
+    NSInteger sections = [self.dataSource numberOfSectionsInTableView:self];
+    NSInteger dataCounts = 0;
+    for (int i = 0; i < sections; i++) {
+        dataCounts += [self.dataSource tableView:self numberOfRowsInSection:i];
+    }
+    return dataCounts > 0 ? YES : NO;
+}
+
+-(void)setPlaceHolderView:(UIView *)placeHolderView
+{
+    objc_setAssociatedObject(self, @selector(placeHolderView), placeHolderView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(UIView *)placeHolderView
+{
+    return objc_getAssociatedObject(self, _cmd);
+}
+
 @end
