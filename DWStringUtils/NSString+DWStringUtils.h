761adebd46644cd900e61f4f4d80559aaad442c2
Wicky <codeWicky@163.com>
2017-04-12 10:41:45 +0800

    String工具类添加文件名修复及随机字符串api，添加必要注释

--- DWStringUtils/NSString+DWStringUtils.h
+++ DWStringUtils/NSString+DWStringUtils.h
@@ -6,6 +6,18 @@
 //  Copyright © 2017年 Wicky. All rights reserved.
 //
 
+/**
+ DWStringUtils
+ NSString工具类
+ 
+ version 1.0.0
+ 提供生成连续相同字符组成的指定长度字符串api
+ 提供根据字体及限制尺寸返回文本尺寸api
+ 提供以指定长度生成随机字符串的api
+ 提供文件名序数修复api
+ 
+ */
+
 #import <UIKit/UIKit.h>
 
 @interface NSString (DWStringUtils)
@@ -20,4 +32,10 @@
 -(CGSize)stringSizeWithFont:(UIFont *)font
                  widthLimit:(CGFloat)widthLimit
                 heightLimit:(CGFloat)heightLimit;
+
+///以长度生成随机字符串，字符串有大小写字母及数字组成
++(NSString *)stringWithRandomCharacterWithLength:(NSUInteger)length;
+
+///给文件名添加序数（文件名重复时使用）
+-(NSString *)dw_FixFileNameStringWithIndex:(NSUInteger)idx;
 @end
--- DWStringUtils/NSString+DWStringUtils.m
+++ DWStringUtils/NSString+DWStringUtils.m
@@ -21,4 +21,27 @@
 {
     return  [self boundingRectWithSize:CGSizeMake(widthLimit, heightLimit) options: NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName:font} context:nil].size;
 }
+
++(NSString *)stringWithRandomCharacterWithLength:(NSUInteger)length {
+    char data[length];
+    for (int i = 0; i < length; i ++) {
+        int ran = arc4random() % 62;
+        if (ran < 10) {
+            ran += 48;
+        } else if (ran < 36) {
+            ran += 55;
+        } else {
+            ran += 61;
+        }
+        data[i] = (char)ran;
+    }
+    return [[NSString alloc] initWithBytes:data length:length encoding:NSUTF8StringEncoding];
+}
+
+-(NSString *)dw_FixFileNameStringWithIndex:(NSUInteger)idx {
+    NSString * extention = [self pathExtension];
+    NSString * pureStr = [self stringByDeletingPathExtension];
+    pureStr = [pureStr stringByAppendingString:[NSString stringWithFormat:@"_%02lu",idx]];
+    return [pureStr stringByAppendingPathExtension:extention];
+}
 @end
