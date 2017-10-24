8359c07b426b25968347a39b997552b5f2012ead
Wicky <codeWicky@163.com>
2017-10-24 14:23:52 +0800

    注释

--- DWDataUtils/NSData+DWDataUtils.h
+++ DWDataUtils/NSData+DWDataUtils.h
@@ -19,8 +19,21 @@
 #import <Foundation/Foundation.h>
 
 @interface NSData (DWDataEncodeUtils)
+
+///解码一个base64字符串
 +(NSData *)dw_DecodeDataFromBase64String:(NSString *)aString;
+
+///将data进行base64编码
 -(NSString *)dw_Base64EncodedString;
+
+///解码一个webSafe的base64字符串
 +(NSData *)dw_WebSafeDecodeDataFromBase64String:(NSString *)aString;
+
+
+/**
+ 将data进行webSafe的base64编码
+
+ @param padding 转码是是否用空格占位空出的两字节
+ */
 -(NSString *)dw_WebSafeBase64EncodedStringWithPadding:(BOOL)padding;
 @end
