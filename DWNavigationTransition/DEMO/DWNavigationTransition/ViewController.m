34eb6eaf84deb776630d96824d2b7f3b2e34cc0d
Wicky <codewicky@163.com>
2019-06-30 16:44:16 +0800

    blank

--- DWNavigationTransition/DEMO/DWNavigationTransition/ViewController.m
+++ DWNavigationTransition/DEMO/DWNavigationTransition/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor yellowColor];
+    
 //    [self.navigationController setNavigationBarHidden:YES animated:YES];
 }
 
