fba5e867563cd6c9ca44352785db1c61ea1a119e
Wicky <codewicky@163.com>
2019-09-26 00:46:42 +0800

    device

--- DWDeviceUtils/UIDevice+DWDeviceUtils.m
+++ DWDeviceUtils/UIDevice+DWDeviceUtils.m
@@ -10,55 +10,56 @@
 #import <sys/utsname.h>
 #import <CoreTelephony/CTCarrier.h>
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <Security/Security.h>
 
 static NSDictionary * infoDic = nil;
 
 @implementation UIDevice (DWDeviceSystemInfo)
 
-+(NSString *)dw_ProjectBuildNo {
++(NSString *)dw_projectBuildNo {
     return getInfoFromSystemInfoDic(CFBridgingRelease(kCFBundleVersionKey));
 }
 
-+(NSString *)dw_ProjectBundleId {
++(NSString *)dw_projectBundleId {
     return getInfoFromSystemInfoDic(CFBridgingRelease(kCFBundleIdentifierKey));
 }
 
-+(NSString *)dw_ProjectDisplayName {
++(NSString *)dw_projectDisplayName {
     return getInfoFromSystemInfoDic(CFBridgingRelease(kCFBundleExecutableKey));
 }
 
-+(NSString *)dw_ProjectVersion {
++(NSString *)dw_projectVersion {
     return getInfoFromSystemInfoDic(@"CFBundleShortVersionString");
 }
 
-+(NSString *)dw_DeviceUUID {
-    return [[UIDevice currentDevice] identifierForVendor].UUIDString;
++(NSString *)dw_deviceUUID {
+    return loadUUID();
 }
 
-+(NSString *)dw_DeviceUserName {
++(NSString *)dw_deviceUserName {
     return [UIDevice currentDevice].name;
 }
 
-+(NSString *)dw_DeviceName {
++(NSString *)dw_deviceName {
     return [UIDevice currentDevice].systemName;
 }
 
-+(NSString *)dw_DeviceSystemVersion {
++(NSString *)dw_deviceSystemVersion {
     return [UIDevice currentDevice].systemVersion;
 }
 
-+(NSString *)dw_DeviceModel {
++(NSString *)dw_deviceModel {
     return [UIDevice currentDevice].model;
 }
 
-+(NSString *)dw_DevicePlatform {
++(NSString *)dw_devicePlatform {
     struct utsname systemInfo;
     uname(&systemInfo);
     return [NSString stringWithCString: systemInfo.machine encoding:NSASCIIStringEncoding];
 }
 
-+(NSString *)dw_DeviceDetailModel {
-    NSString * platform = [self dw_DevicePlatform];
++(NSString *)dw_deviceDetailModel {
+    NSString * platform = [self dw_devicePlatform];
     if([platform isEqualToString:@"iPhone1,1"])  return @"iPhone 2G";
     if([platform isEqualToString:@"iPhone1,2"])  return @"iPhone 3G";
     if([platform isEqualToString:@"iPhone2,1"])  return @"iPhone 3GS";
@@ -87,6 +88,10 @@ static NSDictionary * infoDic = nil;
     if([platform isEqualToString:@"iPhone10,5"]) return @"iPhone 8 Plus";
     if([platform isEqualToString:@"iPhone10,3"]) return @"iPhone X";
     if([platform isEqualToString:@"iPhone10,6"]) return @"iPhone X";
+    if([platform isEqualToString:@"iPhone11,2"]) return @"iPhone XS";
+    if([platform isEqualToString:@"iPhone11,4"]) return @"iPhone XS MAX";
+    if([platform isEqualToString:@"iPhone11,6"]) return @"iPhone XS MAX";
+    if([platform isEqualToString:@"iPhone11,8"]) return @"iPhone XR";
     if([platform isEqualToString:@"iPod1,1"])  return @"iPod Touch 1G";
     if([platform isEqualToString:@"iPod2,1"])  return @"iPod Touch 2G";
     if([platform isEqualToString:@"iPod3,1"])  return @"iPod Touch 3G";
@@ -128,8 +133,8 @@ static NSDictionary * infoDic = nil;
     return platform;
 }
 
-+(NSString *)dw_DeviceCPUType {
-    NSString * platform = [self dw_DevicePlatform];
++(NSString *)dw_deviceCPUType {
+    NSString * platform = [self dw_devicePlatform];
     if([platform isEqualToString:@"iPhone1,1"])  return @"ARMv6";
     if([platform isEqualToString:@"iPhone1,2"])  return @"ARMv6";
     if([platform isEqualToString:@"iPhone2,1"])  return @"ARMv7";
@@ -158,6 +163,10 @@ static NSDictionary * infoDic = nil;
     if([platform isEqualToString:@"iPhone10,5"]) return @"ARMv8-A";
     if([platform isEqualToString:@"iPhone10,3"]) return @"ARMv8-A";
     if([platform isEqualToString:@"iPhone10,6"]) return @"ARMv8-A";
+    if([platform isEqualToString:@"iPhone11,2"]) return @"ARM64e";
+    if([platform isEqualToString:@"iPhone11,4"]) return @"ARM64e";
+    if([platform isEqualToString:@"iPhone11,6"]) return @"ARM64e";
+    if([platform isEqualToString:@"iPhone11,8"]) return @"ARM64e";
     if([platform isEqualToString:@"iPod1,1"])  return @"ARMv6";
     if([platform isEqualToString:@"iPod2,1"])  return @"ARMv6";
     if([platform isEqualToString:@"iPod3,1"])  return @"ARMv7";
@@ -197,7 +206,7 @@ static NSDictionary * infoDic = nil;
     return platform;
 }
 
-+(CGFloat)dw_DeviceTotalMemory {
++(CGFloat)dw_deviceTotalMemory {
     float size = 0.0;
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSDictionary *dictionary = [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject] error: nil];
@@ -208,7 +217,7 @@ static NSDictionary * infoDic = nil;
     return size;
 }
 
-+(CGFloat)dw_DeviceFreeMemory {
++(CGFloat)dw_deviceFreeMemory {
     float size = 0.0;
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSDictionary *dictionary = [[NSFileManager defaultManager] attributesOfFileSystemForPath:[paths lastObject] error: nil];
@@ -220,23 +229,128 @@ static NSDictionary * infoDic = nil;
     return size;
 }
 
-+(NSString *)dw_MobileOperator {
++(NSString *)dw_mobileOperator {
     CTTelephonyNetworkInfo * info = [[CTTelephonyNetworkInfo alloc] init];
     CTCarrier * carrier = [info subscriberCellularProvider];
     NSString * mCarrier = [NSString stringWithFormat:@"%@",[carrier carrierName]];
     return mCarrier;
 }
 
-+(NSString *)dw_DevelopSDKVersion {
++(NSString *)dw_developSDKVersion {
     return getInfoFromSystemInfoDic(@"DTSDKBuild");
 }
 
-static inline id getInfoFromSystemInfoDic(NSString * key) {
++(CGFloat)dw_batteryVolumn {
+    [UIDevice currentDevice].batteryMonitoringEnabled = YES;
+    double deviceLevel = [UIDevice currentDevice].batteryLevel;
+    [UIDevice currentDevice].batteryMonitoringEnabled = NO;
+    return deviceLevel;
+}
+
+#pragma mark --- tool func ---
+NS_INLINE id getInfoFromSystemInfoDic(NSString * key) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         infoDic = [[NSBundle mainBundle] infoDictionary];
     });
     return [infoDic objectForKey:key];
+}
+
+NS_INLINE NSString * uuidString() {
+    CFUUIDRef uuidRef = CFUUIDCreate(NULL);
+    CFStringRef uuidStringRef = CFUUIDCreateString(NULL, uuidRef);
+    CFRelease(uuidRef);
+    
+    NSString *uuidValue = (__bridge_transfer NSString *)uuidStringRef;
+    uuidValue = [uuidValue lowercaseString];
+    uuidValue = [uuidValue stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    return uuidValue;
+}
+
+NS_INLINE NSString * loadUUID() {
+    NSString * uuid = nil;
+    NSString * storeKey = uuidKey();
+    NSUserDefaults * ud = [NSUserDefaults standardUserDefaults];
+    uuid = [ud valueForKey:storeKey];
+    
+    if (uuid.length) {
+        return uuid;
+    }
+
+    uuid = loadUUIDFromKeyChain(storeKey);
+
+    if (uuid.length) {
+        [ud setValue:uuid forKey:storeKey];
+        [ud synchronize];
+        return uuid;
+    }
+    
+    uuid = uuidString();
+    if (uuid) {
+        [ud setValue:uuid forKey:storeKey];
+        [ud synchronize];
+        saveUUIDToKeyChain(uuid, storeKey);
+    }
+    return uuid;
+}
+
+NS_INLINE NSString * uuidKey() {
+    return [NSString stringWithFormat:@"uuid-for-%@",[UIDevice dw_projectBundleId]];
+}
+
+NS_INLINE NSString * loadUUIDFromKeyChain(NSString * storeKey) {
+    if (!storeKey.length) {
+        return nil;
+    }
+    
+    CFTypeRef result = NULL;
+    NSMutableDictionary *query = queryDictionary(storeKey);
+    [query setObject:@YES forKey:(__bridge id)kSecReturnData];
+    [query setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
+    if (status != errSecSuccess) {
+        return nil;
+    }
+    
+    NSData * data = (__bridge_transfer NSData *)result;
+    if ([data length]) {
+        return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    return nil;
+}
+
+NS_INLINE void saveUUIDToKeyChain(NSString * uuid,NSString * storeKey) {
+    
+    if (!uuid.length || !storeKey.length) {
+        return;
+    }
+    
+    NSData * uuidData = [uuid dataUsingEncoding:NSUTF8StringEncoding];
+    if (!uuidData.length) {
+        return;
+    }
+    
+    NSMutableDictionary *query = nil;
+    NSMutableDictionary *searchQuery = queryDictionary(storeKey);
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)searchQuery, nil);
+    if (status == errSecSuccess) {
+        query = [NSMutableDictionary dictionary];
+        [query setObject:uuidData forKey:(__bridge id)kSecValueData];
+        status = SecItemUpdate((__bridge CFDictionaryRef)(searchQuery), (__bridge CFDictionaryRef)(query));
+    } else if (status == errSecItemNotFound){
+        query = queryDictionary(storeKey);
+        [query setObject:uuidData forKey:(__bridge id)kSecValueData];
+        status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+    }
+}
+
+NS_INLINE NSMutableDictionary * queryDictionary(NSString * storeKey) {
+    NSMutableDictionary * query = [NSMutableDictionary dictionary];
+    [query setObject:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
+    [query setObject:storeKey forKey:(__bridge id)kSecAttrService];
+    [query setObject:storeKey forKey:(__bridge id)kSecAttrAccount];
+    [query setObject:(__bridge id)(kSecAttrSynchronizableAny) forKey:(__bridge id)(kSecAttrSynchronizable)];
+    return query;
 }
 
 @end
